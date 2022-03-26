c8b454f5d5fa4c967150e510f1d3ca9581b407a2
KsKarim <kskarim177@gmail.com>
2022-03-26 18:45:40 +0600

    the second commit

--- src/Components/QnA/written.js
+++ src/Components/QnA/written.js
@@ -3,7 +3,8 @@ import React from 'react';
 const written = () => {
     return (
         <div>
-            
+            <h1>How React Work? </h1>
+            <p>-ReactJS is simply a JavaScript library for building user interfaces. It works in declarative code. </p>
         </div>
     );
 };
